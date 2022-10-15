11955bcbb257afd9f22d6265d89fdd58a67958bc
Petromir Kolev <petromirkolev@gmail.com>
2022-10-15 14:20:39 +0300

    Finish view and bookmark book fn

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 // Global variables
-let books = [];
-let bookmarks = [];
-const bookDisplay = document.querySelector(".book-library");
-const libraryContainer = document.querySelector(".library-container");
-const addForm = document.querySelector(".add-book-form");
-const editForm = document.querySelector(".edit-book-form");
-const deleteForm = document.querySelector(".delete-book-form");
+let books = [
+  {
+    id: 1,
+    title: "My book cover",
+    author: "My name here",
+    url: "https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg",
+    read: true,
+  },
+  {
+    id: 2,
+    title: "Lunar Storm",
+    author: "Terry Crosby",
+    url: "https://www.adobe.com/express/create/cover/media_178ebed46ae02d6f3284c7886e9b28c5bb9046a02.jpeg",
+    read: false,
+  },
+];
+let bookmarks = [
+  {
+    id: 1,
+    title: "My book cover",
+    author: "My name here",
+    url: "https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg",
+    read: true,
+  },
+];
+const query = document.querySelector.bind(document);
+const bookDisplay = query(".book-library");
+const libraryContainer = query(".library-container");
+const addForm = query(".add-book-form");
+const editForm = query(".edit-book-form");
+const deleteForm = query(".delete-book-form");
+const viewForm = query(".view-book-form");
+const bookmarkedBooks = query(".bookmarked-books");
+const bookmarkForm = query(".bookmark-book-form");
 
-// Create book object constructor
+// Define book constructor model
 class Book {
   constructor(title, author, url) {
     this.id = books.length + 1;
@@ -17,11 +44,11 @@ class Book {
     this.read = false;
   }
 }
-// Create new book object and add it to books array
+// Create new book object and add it to books []
 const createBook = function (title, author, url) {
   books.push(new Book(title, author, url));
 };
-// Check which button is pressed
+// Button clicked controller
 libraryContainer.addEventListener("click", (e) => {
   const currentId = e.target.classList[1];
   switch (e.target.classList[0]) {
@@ -39,55 +66,68 @@ libraryContainer.addEventListener("click", (e) => {
       return;
   }
 });
-// Display all books on home page
+// Display all books on the home page
 const renderBooks = () => {
   bookDisplay.innerHTML = "";
-  console.log(books);
   books.map((book) => displayBook(book));
 };
-// Add new book
+// Add new book to books []
 const addNewBook = () => {
   addForm.style.visibility = "visible";
-  const title = document.querySelector(".inp-add-title");
-  const author = document.querySelector(".inp-add-author");
-  const url = document.querySelector(".inp-add-url");
-  document
-    .querySelector(".btn-add-book")
-    .addEventListener("click", function addBook(e) {
-      e.preventDefault();
-      createBook(title.value, author.value, url.value);
-      title.value = author.value = url.value = "";
-      renderBooks();
-      addForm.style.visibility = "hidden";
-      document
-        .querySelector(".btn-add-book")
-        .removeEventListener("click", addBook);
-    });
+  const [title, author, url] = [
+    query(".inp-add-title"),
+    query(".inp-add-author"),
+    query(".inp-add-url"),
+  ];
+  query(".btn-add-book").addEventListener("click", function addBook(e) {
+    e.preventDefault();
+    createBook(title.value, author.value, url.value);
+    title.value = author.value = url.value = "";
+    renderBooks();
+    addForm.style.visibility = "hidden";
+    query(".btn-add-book").removeEventListener("click", addBook);
+  });
 };
 // View existing book
-const viewBook = (currentId) => {};
-
+const viewBook = (currentId) => {
+  books.map((book) => {
+    if (book.id !== +currentId) return;
+    viewForm.style.visibility = "visible";
+    query(".view-book-img").src = book.url;
+    query(".read-status").textContent = book.read ? "Yes" : "No";
+    query(".btn-close-view-book").addEventListener("click", (e) => {
+      e.preventDefault();
+      viewForm.style.visibility = "hidden";
+    });
+  });
+};
 // Edit existing book
 const editBook = (currentId) => {
   books.map((book) => {
     if (book.id !== +currentId) return;
     editForm.style.visibility = "visible";
-    document.querySelector("#edit-title").value = book.title;
-    document.querySelector("#edit-author").value = book.author;
-    document.querySelector("#edit-url").value = book.url;
-    document
-      .querySelector(".btn-edit-book")
-      .addEventListener("click", function editCurrentBook(e) {
+    [
+      query("#edit-title").value,
+      query("#edit-author").value,
+      query("#edit-url").value,
+      query("#edit-status").checked,
+    ] = [book.title, book.author, book.url, book.read];
+
+    query(".btn-edit-book").addEventListener(
+      "click",
+      function editCurrentBook(e) {
         e.preventDefault();
-        book.title = document.querySelector("#edit-title").value;
-        book.author = document.querySelector("#edit-author").value;
-        book.url = document.querySelector("#edit-url").value;
+        [book.title, book.author, book.url, book.read] = [
+          query("#edit-title").value,
+          query("#edit-author").value,
+          query("#edit-url").value,
+          query("#edit-status").checked,
+        ];
         renderBooks();
         editForm.style.visibility = "hidden";
-        document
-          .querySelector(".btn-edit-book")
-          .removeEventListener("click", editCurrentBook);
-      });
+        query(".btn-edit-book").removeEventListener("click", editCurrentBook);
+      }
+    );
   });
 };
 // Delete existing book
@@ -95,48 +135,70 @@ const deleteBook = (currentId) => {
   books.map((book) => {
     if (book.id !== +currentId) return;
     deleteForm.style.visibility = "visible";
-    document
-      .querySelector(".btn-delete-book")
-      .addEventListener("click", function deleteCurrentBook(e) {
+    query(".delete-book-img").src = book.url;
+    query(".btn-delete-book").addEventListener(
+      "click",
+      function deleteCurrentBook(e) {
         e.preventDefault();
         const index = books.findIndex((book) => book.id === +currentId);
         books.splice(index, 1);
         deleteForm.style.visibility = "hidden";
         renderBooks();
-        document
-          .querySelector(".btn-delete-book")
-          .removeEventListener("click", deleteCurrentBook);
-      });
+        query(".btn-delete-book").removeEventListener(
+          "click",
+          deleteCurrentBook
+        );
+      }
+    );
   });
 };
 // Bookmark existing book
-const bookmarkBook = (currentId) => {};
+const bookmarkBook = (currentId) => {
+  // Render bookmarks preview
+  bookmarkForm.style.visibility = "visible";
+  bookmarkedBooks.innerHTML = "";
+  bookmarks.map((book) => {
+    bookmarkedBooks.insertAdjacentHTML(
+      "beforeend",
+      `<div class="bkm-book">
+        <img class="bookmarks-book-img" src="${book.url}" alt="Cover" />
+      </div>`
+    );
+  });
+  const index = books.findIndex((book) => book.id === +currentId);
+  query(".bookmark-current-book").src = books[index].url;
+  // Handle bookmark logic
+  query(".btn-bookmark-book").addEventListener(
+    "click",
+    function bookmarkCurrentBook(e) {
+      e.preventDefault();
+      let isBookmarked;
+      bookmarks.map((book) => {
+        if (book.id === +currentId) isBookmarked = true;
+      });
+      if (!isBookmarked) bookmarks.push(books[index]);
+      bookmarkForm.style.visibility = "hidden";
+      query(".btn-bookmark-book").removeEventListener(
+        "click",
+        bookmarkCurrentBook
+      );
+    }
+  );
+};
 // Single book view
 const displayBook = (book) => {
   bookDisplay.insertAdjacentHTML(
     "afterbegin",
     ` <div class="book">
-              <img src="${book.url}" alt="Cover" />
-              <div class="book-btn-container">
-              <button class="book-view ${book.id}">View</button>
-              <button class="book-edit ${book.id}">Edit</button>
-              <button class="book-delete ${book.id}">Delete</button>
-              <button class="book-bookmark ${book.id}">Bookmark</button>
-              </div>
-        </div>`
+        <img src="${book.url}" alt="Cover" />
+        <div class="book-btn-container">
+          <button class="book-view ${book.id}">View</button>
+          <button class="book-edit ${book.id}">Edit</button>
+          <button class="book-delete ${book.id}">Delete</button>
+          <button class="book-bookmark ${book.id}">Bookmark</button>
+        </div>
+      </div>`
   );
 };
-
-createBook(
-  "Az",
-  "Ti",
-  "https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg"
-);
-createBook(
-  "toi",
-  "tq",
-  "https://www.adobe.com/express/create/cover/media_178ebed46ae02d6f3284c7886e9b28c5bb9046a02.jpeg"
-);
-
 // Initialize library
 renderBooks();
